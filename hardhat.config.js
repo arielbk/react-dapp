999618de01e46df8d8a058af1d141debd0a471fb
arielbk <buchwald.kearney@gmail.com>
2021-12-11 20:15:46 +0100

    feat: contract read and write on ropsten testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
-require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-waffle')
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -20,11 +20,15 @@ module.exports = {
   solidity: '0.8.4',
   paths: {
     // artifacts compile into the src directory
-    artifacts: './src/artifacts',
+    artifacts: './artifacts',
   },
   networks: {
     hardhat: {
       chainId: 1337,
     },
+    ropsten: {
+      url: 'https://ropsten.infura.io/v3/ddbf381413c649c8a1d2fc18168cc793',
+      accounts: [`0x${process.env.ACCOUNT_KEY}`],
+    },
   },
-};
+}
