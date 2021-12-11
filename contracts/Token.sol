57c3e2d0cb849812a96291d55d64b5cba49e035f
arielbk <buchwald.kearney@gmail.com>
2021-12-11 21:26:16 +0100

    feat: add basic token transactions

---
+++ contracts/Token.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+  string public name = "arielbk token";
+  string public symbol = "ABKT";
+  uint public totalSupply = 1000;
+  mapping (address => uint) balances;
+
+  constructor() {
+    balances[msg.sender] = totalSupply;
+  }
+
+  function transfer(address to, uint amount) external {
+    require(balances[msg.sender] >= amount, "Not enough tokens");
+    balances[msg.sender] -= amount;
+    balances[to] += amount;
+  }
+
+  function balanceOf(address account) external view returns (uint) {
+    return balances[account];
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 import { ethers } from 'ethers'
 import Greeter from '../artifacts/contracts/Greeter.sol/Greeter.json'
+import Token from '../artifacts/contracts/Token.sol/Token.json'
 
 const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const tokenAddress = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
 
 export default function Home() {
   const [greetingValue, setGreetingValue] = useState('')
+  const [userAccount, setUserAccount] = useState('')
+  const [amount, setAmount] = useState(0)
 
   // request access to the user's MetaMask account
   async function requestAccount() {
@@ -48,6 +51,34 @@ export default function Home() {
     }
   }
 
+  // get current user token balance
+  async function getBalance() {
+    if (typeof window.ethereum === 'undefined') {
+      return
+    }
+    const [account] = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+    const balance = await contract.balanceOf(account)
+    console.log('Balance:', balance.toString())
+  }
+
+  // send token to another user
+  async function sendTokens() {
+    if (typeof window.ethereum === 'undefined') {
+      return
+    }
+    console.log(`Sending ${amount} tokens to account ${userAccount} ...`)
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(tokenAddress, Token.abi, signer)
+    const transaction = await contract.transfer(userAccount, amount)
+    await transaction.wait()
+    console.log(`${amount} tokens sent to ${userAccount}`)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -67,6 +98,17 @@ export default function Home() {
           onClick={(e) => setGreetingValue(e.target.value)}
           placeholder="Greeting text"
         />
+        <hr />
+        <button onClick={getBalance}>Get balance</button>
+        <input
+          onClick={(e) => setUserAccount(e.target.value)}
+          placeholder="User Account"
+        />
+        <input
+          onClick={(e) => setAmount(e.target.value)}
+          placeholder="Number of abk tokens"
+        />
+        <button onClick={sendTokens}>Send tokens</button>
       </main>
     </div>
   )
