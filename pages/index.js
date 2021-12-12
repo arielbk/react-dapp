74ce027fdbc20281fd4c3186d0e67e9b27439379
arielbk <buchwald.kearney@gmail.com>
2021-12-12 14:58:46 +0100

    feat: add a bearable ui with loading states

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,41 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import {
+  Flex,
+  Heading,
+  IconButton,
+  Input,
+  Stack,
+  useTheme,
+} from '@chakra-ui/react'
 import { ethers } from 'ethers'
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import { Button, Box } from '@chakra-ui/react'
+import { useColorMode, InputLeftElement, InputGroup } from '@chakra-ui/react'
 import Greeter from '../artifacts/contracts/Greeter.sol/Greeter.json'
 import Token from '../artifacts/contracts/Token.sol/Token.json'
+import styles from '../styles/Home.module.css'
+import { BsMoonFill, BsFillSunFill } from 'react-icons/bs'
+import { MdOutlineAccountCircle } from 'react-icons/md'
+import { BiCoin, BiMessageRounded } from 'react-icons/bi'
+import { GiWavyChains } from 'react-icons/gi'
 
 const greeterAddress = '0x8Cdb2Ab9C7F6019787ed1878EB05D1467E48Ab83'
 const tokenAddress = '0xbb0c8942e03542E2eB28F49F1763412caC0C50f3'
 
 export default function Home() {
+  const [chainGreeting, setChainGreeting] = useState('')
+  const [isFetchingGreeting, setIsFetchingGreeting] = useState(false)
   const [greetingValue, setGreetingValue] = useState('')
-  const [userAccount, setUserAccount] = useState('')
+  const [isSettingGreeting, setIsSettingGreeting] = useState(false)
+
+  const [balance, setBalance] = useState('')
+  const [isGettingBalance, setIsGettingBalance] = useState(false)
+  const [toAddress, setToAddress] = useState('')
   const [amount, setAmount] = useState(0)
+  const [isSending, setIsSending] = useState(false)
+
+  const { colorMode, toggleColorMode } = useColorMode()
+  const chakraTheme = useTheme()
 
   // request access to the user's MetaMask account
   async function requestAccount() {
@@ -20,25 +44,24 @@ export default function Home() {
 
   // call the smart contract, read the current greeting value
   async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(
-        greeterAddress,
-        Greeter.abi,
-        provider
-      )
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-      } catch (err) {
-        console.log('Error: ', err)
-      }
+    if (!window.ethereum) return
+    setIsFetchingGreeting(true)
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+    try {
+      const data = await contract.greet()
+      setChainGreeting(data)
+      setIsFetchingGreeting(false)
+    } catch (err) {
+      console.log('Error: ', err)
+      setIsFetchingGreeting(false)
     }
   }
 
   // call the smart contract, send an update
   async function setGreeting() {
     if (!greetingValue || !window.ethereum) return
+    setIsSettingGreeting(true)
     await requestAccount()
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
@@ -46,12 +69,13 @@ export default function Home() {
     const transaction = await contract.setGreeting(greetingValue)
     await transaction.wait()
     setGreetingValue('')
-    fetchGreeting()
+    setIsSettingGreeting(false)
   }
 
   // get current user token balance
   async function getBalance() {
     if (!window.ethereum) return
+    setIsGettingBalance(true)
     const [account] = await window.ethereum.request({
       method: 'eth_requestAccounts',
     })
@@ -59,20 +83,28 @@ export default function Home() {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
     const balance = await contract.balanceOf(account)
-    console.log('Balance:', balance.toString())
+    setBalance(balance.toString())
+    setIsGettingBalance(false)
   }
 
   // send token to another user
   async function sendTokens() {
     if (!amount || !window.ethereum) return
-    console.log(`Sending ${amount} tokens to account ${userAccount} ...`)
+    setIsSending(true)
+    console.log(`Sending ${amount} tokens to account ${toAddress} ...`)
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(tokenAddress, Token.abi, signer)
-    const transaction = await contract.transfer(userAccount, amount)
+    const transaction = await contract.transfer(toAddress, amount)
     await transaction.wait()
-    console.log(`${amount} tokens sent to ${userAccount}`)
+    console.log(`${amount} tokens sent to ${toAddress}`)
+    setIsSending(false)
   }
+
+  // fetch initial greeting on load
+  useEffect(() => {
+    fetchGreeting()
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -86,24 +118,125 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>React dApp</h1>
-        <button onClick={fetchGreeting}>Fetch greeting</button>
-        <button onClick={setGreeting}>Set greeting</button>
-        <input
-          onClick={(e) => setGreetingValue(e.target.value)}
-          placeholder="Greeting text"
-        />
-        <hr />
-        <button onClick={getBalance}>Get balance</button>
-        <input
-          onClick={(e) => setUserAccount(e.target.value)}
-          placeholder="User Account"
-        />
-        <input
-          onClick={(e) => setAmount(e.target.value)}
-          placeholder="Number of abk tokens"
-        />
-        <button onClick={sendTokens}>Send tokens</button>
+        <Flex
+          alignItems={'center'}
+          justifyContent={'space-between'}
+          mb={8}
+          width="100%"
+          maxWidth="500px"
+        >
+          <Heading>
+            <Box display="inline-block" mr="4" color="blue.400">
+              <GiWavyChains />
+            </Box>
+            React dApp
+          </Heading>
+          <IconButton
+            icon={colorMode === 'light' ? <BsFillSunFill /> : <BsMoonFill />}
+            onClick={toggleColorMode}
+          />
+        </Flex>
+        <Stack spacing={4} width="100%" maxWidth="500px">
+          <Stack
+            borderWidth={1}
+            p={4}
+            borderRadius={12}
+            spacing={4}
+            _hover={{
+              boxShadow: '0 5px 30px rgba(0,0,0,0.1)',
+              transition: '0.3s',
+            }}
+          >
+            <Heading
+              size="md"
+              p={4}
+              textAlign={'center'}
+              display="flex"
+              justifyContent="space-around"
+            >
+              <Box>Contract greeting:</Box>
+              <Box textDecoration={'underline'}>{chainGreeting}</Box>
+            </Heading>
+            <Button onClick={fetchGreeting} isLoading={isFetchingGreeting}>
+              Fetch greeting
+            </Button>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Box fontSize="1.3rem" opacity="0.3">
+                  <BiMessageRounded />
+                </Box>
+              </InputLeftElement>
+              <Input
+                onClick={(e) => setGreetingValue(e.target.value)}
+                placeholder="Greeting text"
+              />
+            </InputGroup>
+            <Button
+              onClick={setGreeting}
+              isLoading={isSettingGreeting}
+              colorScheme={'blue'}
+              variant={'outline'}
+            >
+              Set greeting
+            </Button>
+          </Stack>
+          <Stack
+            borderWidth={1}
+            p={4}
+            spacing={4}
+            borderRadius={12}
+            _hover={{
+              boxShadow: '0 5px 30px rgba(0,0,0,0.1)',
+              transition: '0.3s',
+            }}
+          >
+            <Heading
+              size="md"
+              py={4}
+              textAlign={'center'}
+              display="flex"
+              justifyContent="space-around"
+            >
+              <Box>Token balance:</Box>
+              <Box textDecoration={'underline'}>
+                {balance || 'Get balance 👇'}
+              </Box>
+            </Heading>
+            <Button onClick={getBalance} isLoading={isGettingBalance}>
+              Get balance
+            </Button>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Box fontSize="1.3rem" opacity="0.3">
+                  <MdOutlineAccountCircle />
+                </Box>
+              </InputLeftElement>
+              <Input
+                onChange={(e) => setToAddress(e.target.value)}
+                placeholder="To account"
+              />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Box fontSize="1.3rem" opacity="0.3">
+                  <BiCoin />
+                </Box>
+              </InputLeftElement>
+              <Input
+                onClick={(e) => setAmount(e.target.value)}
+                placeholder="Number of abk tokens"
+              />
+            </InputGroup>
+            <Button
+              onClick={sendTokens}
+              isLoading={isSending}
+              colorScheme="blue"
+              variant={'outline'}
+            >
+              Send tokens
+            </Button>
+          </Stack>
+        </Stack>
       </main>
     </div>
   )
