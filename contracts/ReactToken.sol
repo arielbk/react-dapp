03a8f6dd30cd789b668540e51343bee17526c8d9
arielbk <buchwald.kearney@gmail.com>
2021-12-18 11:26:59 +0100

    Switch to React ERC20 token

--- contracts/ReactToken.sol
+++ contracts/ReactToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ABKToken is ERC20 {
-  constructor() ERC20("ABK Token", "ABKT") {
+contract ReactToken is ERC20 {
+  constructor() ERC20("React Token", "RCT") {
     _mint(msg.sender, 100000 * 10**18);
   }
 }
